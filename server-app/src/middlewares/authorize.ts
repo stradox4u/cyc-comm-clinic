d0687daee627d8d600a86ec317552a5062480e57
Cysteine12 <gbengapeace2001@gmail.com>
2025-07-12 05:59:35 +0100

    wip: auth setup

--- server-app/src/middlewares/authorize.ts
+++ server-app/src/middlewares/authorize.ts
@@ -1,16 +1,16 @@
 import type { NextFunction, Request, Response } from 'express'
 import { UnauthenticatedError, UnauthorizedError } from './errorHandler.js'
-// import { AdminRole } from '@prisma/client'
+import { ProviderRoleTitle } from '@prisma/client'
 
-const authorize = (roles: AdminRole[]) => {
+const authorize = (roleTitles: ProviderRoleTitle[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!req.user) {
       throw new UnauthenticatedError('You need to login first')
     }
 
-    const adminRole = req.user.role
+    const adminRole = req.user.role_title
 
-    if (!roles.includes(adminRole)) {
+    if (!roleTitles.includes(adminRole)) {
       throw new UnauthorizedError("You don't have the permission for access")
     }
     next()
