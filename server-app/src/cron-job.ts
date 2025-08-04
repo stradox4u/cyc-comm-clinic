1ceaac0ad21355083ba112763c87130c58d43bbd
Cysteine12 <gbengapeace2001@gmail.com>
2025-08-04 07:07:28 +0100

    feat(cron-job): flag missed appointment as no-show

--- server-app/src/cron-job.ts
+++ server-app/src/cron-job.ts
@@ -1,7 +1,13 @@
 import cron from 'node-cron'
 import logger from './middlewares/logger.js'
 import prisma from './config/prisma.js'
-import type { Appointment, Patient } from '@prisma/client'
+import {
+  AppointmentPurpose,
+  AppointmentStatus,
+  Prisma,
+  type Appointment,
+  type Patient,
+} from '@prisma/client'
 import emailService from './services/email.service.js'
 
 export type AppointmentSchedule = {
@@ -10,8 +16,16 @@ export type AppointmentSchedule = {
   change_count: number
 }
 
+export type NoShowAppointment = {
+  id: string
+  schedule: AppointmentSchedule
+  purposes: AppointmentPurpose[]
+  email: string
+  first_name: string
+}
+
 cron.schedule('0 7 * * *', async () => {
-  logger.info('Running cron job at 7:00AM')
+  logger.info('Running appointment reminder cron job at 7:00AM')
 
   const upcomingAppointments = (await prisma.$queryRaw`
     SELECT a.*, p.*
@@ -36,5 +50,30 @@ cron.schedule('0 7 * * *', async () => {
 
   await Promise.all(emailPromises)
 
-  logger.info('Cron job executed successfully')
+  logger.info('Appointment reminder cron job executed successfully')
+})
+
+cron.schedule('0 19 * * *', async () => {
+  logger.info('Running appointment no-show cron job at 7:00PM')
+
+  const noShowAppointments = await prisma.$queryRaw<
+    NoShowAppointment[]
+  >(Prisma.sql`
+    UPDATE "appointments"
+    SET status = ${AppointmentStatus.NO_SHOW}
+    WHERE status = ${AppointmentStatus.SCHEDULED}
+      AND (schedule->>'appointment_date')::date <= CURRENT_DATE
+    RETURNING id, schedule, purposes, email, first_name
+  `)
+
+  const emailPromises = noShowAppointments.map((appointment) => {
+    return emailService.sendAppointmentResheduleRequestMail(appointment, {
+      email: appointment.email,
+      first_name: appointment.first_name,
+    })
+  })
+
+  await Promise.all(emailPromises)
+
+  logger.info('Appointment no-show cron job executed successfully')
 })
